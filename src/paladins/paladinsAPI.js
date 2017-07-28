306cc21dd6f03fb656ec5963f5b4acee155cc142
Emerson Jacobson <emwjacobson@gmail.com>
2017-07-28 16:52:45 -0700

    Added previous changes to Smite and changed reject.

--- src/paladins/paladinsAPI.js
+++ src/paladins/paladinsAPI.js
@@ -175,7 +175,7 @@ class Paladins {
         if (!error && response.statusCode === 200) {
           resolve(JSON.parse(body))
         } else {
-          reject({body: body, error: error, response: response})
+          reject(response)
         }
       })
     })
